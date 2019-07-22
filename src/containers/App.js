a2442c8a6337015505b017e8cebdc7fce70b8303
Paulina Stefanska <web.paulinastefanska@gmail.com>
2019-07-22 13:17:50 +0200

    Install uuid

---
+++ src/containers/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import uuid from 'uuid';
+
+class App extends React.Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            data: []
+        };
+    }
+    addTodo(val){
+        const todo = {
+            text: val,
+            id: uuid.v4(),
+        };
+        const data = [...this.state.data, todo];
+        this.setState({data});
+    }
+}
+
+export default App;
