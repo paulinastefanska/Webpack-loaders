8e1ae958d14780f0f485185837c61d7cdec22457
Paulina Stefanska <web.paulinastefanska@gmail.com>
2019-07-23 11:14:05 +0200

    Add title component

--- src/components/Title.js
+++ src/components/Title.js
@@ -1,1 +1,15 @@
-const Title = props => <h1>{props.title}</h1>
+import React from 'react';
+import Todo from './Todo'
+
+class Title extends React.Component {
+	render () {
+		return (
+			<div className="title">
+				<h1>To do List:</h1>
+				<ul>{ this.props.data }</ul>
+			</div>	
+		);
+	}
+}
+
+export default Title;
